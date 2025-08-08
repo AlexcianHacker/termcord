b518190bda16145ed011ce7db38c8514ab852f1d
Alex The Alexcían Haxxorman <82779789+AlexcianHacker@users.noreply.github.com>
2025-08-08 16:41:45 -0400

    Update comcord.py

--- main/comcord.py
+++ main/comcord.py
@@ -1,1 +1,17 @@
+import aiohttp; 
+import discord; 
+from discord.ext import commands; 
+import requests; 
 
+
+def main() -> int: 
+  def initialize_tk(token: str, 
+                    prefix: str = None) -> int: 
+    
+    return 
+  def initialize_login(eml: str, 
+                      pswd: str, 
+                      prefix: str = None, 
+                      token: str = None) -> int: 
+    
+    return 
