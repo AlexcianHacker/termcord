acdfd47e3be9703074b0a021188f538d56225b87
Alex The Alexcían Haxxorman <82779789+AlexcianHacker@users.noreply.github.com>
2025-08-10 14:38:47 -0400

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,5 +1,14 @@
-import comcord; 
+from comcord import main; 
+import sys; 
 
 
 if __name__=="__main__": 
+  print("Would You Like To Allow GUI (Y/N)?: "); 
+  result: int; 
+  if 'Y' in sys.stdin.readline(): 
+    print("Booting Discord.... "); 
+    result = main(); 
+  else: 
+    print("Booting Discord.... "); 
+    result = main(panels=False); 
   
