f4a6efab06f9b6c9b826ad058a78a0d8f7faa859
Alex The Alexcían Haxxorman <82779789+AlexcianHacker@users.noreply.github.com>
2025-08-10 14:22:54 -0400

    Update comcord.py

--- main/comcord.py
+++ main/comcord.py
@@ -1,17 +1,27 @@
+import asyncio; 
 import aiohttp; 
 import discord; 
 from discord.ext import commands; 
+import sys; 
+import os; 
 import requests; 
+from itertools import cycle; 
+import random; 
 
 
-def main() -> int: 
-  def initialize_tk(token: str, 
+def main(panels: bool = True) -> int: 
+  if panels: import tkinter; 
+  
+  def initialize_tn(token: str, 
                     prefix: str = None) -> int: 
     
     return 
+  
   def initialize_login(eml: str, 
                       pswd: str, 
                       prefix: str = None, 
                       token: str = None) -> int: 
     
     return 
+  
+  return 0; 
