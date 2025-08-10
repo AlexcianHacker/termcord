51c979896d9a8368363a383f56c523ee13b48ce7
Alex The Alexcían Haxxorman <82779789+AlexcianHacker@users.noreply.github.com>
2025-08-10 16:39:24 -0400

    Update main.py

--- main/main.py
+++ main/main.py
@@ -5,10 +5,15 @@ import sys;
 if __name__=="__main__": 
   print("Would You Like To Allow GUI (Y/N)?: "); 
   result: int; 
-  if 'Y' in sys.stdin.readline(): 
-    print("Booting Discord.... "); 
-    result = main(); 
-  else: 
-    print("Booting Discord.... "); 
-    result = main(panels=False); 
+  pans: bool = True; 
+  if 'Y' in sys.stdin.readline(): pans = True; 
+  else: pans = False; 
+  print("Would You Like To Run In Hybrid Mode [Bot Commands Included] (Y/N)?: "); 
+  hybrid: bool = True; 
+  if 'Y' in sys.stdin.readline(): hybrid = True; 
+  else: hybrid = False; 
+  print("Booting Discord.... "); 
+  result = main(dual=hybrid, panels=pans); 
+  del pans, hybrid; 
+  print("Program Exited With Exit Code ", result, ". "); 
   
