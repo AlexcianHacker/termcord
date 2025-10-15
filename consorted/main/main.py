126f70579363003bbae0a848708f76b073b58d2b
Alex The Alexcían Haxxorman <82779789+AlexcianHacker@users.noreply.github.com>
2025-10-15 00:05:26 -0400

    Update main.py

--- consorted/main/main.py
+++ consorted/main/main.py
@@ -12,8 +12,11 @@ if __name__=="__main__":
   hybrid: bool = True; 
   if 'Y' in sys.stdin.readline(): hybrid = True; 
   else: hybrid = False; 
+  print("Would You Like To Use Simple Mode? (Y/N): "); 
+  simple_m: bool = False; 
+  if 'Y' in sys.stdin.readline(): simple_m = True; 
   print("Booting Discord.... "); 
-  result = main(dual=hybrid, panels=pans); 
+  result = main(dual=hybrid, simple=simple_m, panels=pans); 
   del pans, hybrid; 
   print("Program Exited With Exit Code ", result, ". "); 
   
