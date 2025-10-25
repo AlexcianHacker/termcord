a59b05d7f97b0756f322fa26677a80216ff21de0
Alex The Alexcían Haxxorman <82779789+AlexcianHacker@users.noreply.github.com>
2025-10-24 21:32:48 -0400

    Update types_cord.py

--- consorted/main/types_cord.py
+++ consorted/main/types_cord.py
@@ -3,12 +3,23 @@ import aiohttp
 
 
 class Clientele(): 
+  def __init__(self, 
+               com_prefix: str, 
+               clientUser: bool) -> None: 
+    return None; 
   
-
 
 class User(Clientele): 
+  def __init__(self,) 
   
+  return None; 
+  
+
+
 
 
 class Bot(Clientele): 
+  def __init__(self,) 
+    
+    return None; 
   
